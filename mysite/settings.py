c6b2810cd38b141d03be74938528db03b5544731
Sushil Chaudhary <monster321421@gmail.com>
2025-02-11 21:31:14 +0545

    added db

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polls',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '12345',
         'HOST': 'localhost',
         'PORT': '5432'
     }
